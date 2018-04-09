02d6affcf3bcf4175279b9cd6160ec2d1b82a75c
Usslessvoid <36482404+Usslessvoid@users.noreply.github.com>
2018-04-09 23:42:51 +0300

    2

--- lab4/src/Runner.java
+++ lab4/src/Runner.java
@@ -1,8 +1,4 @@
-import java.util.Arrays;
 import java.util.Scanner;
-
-import by.gsu.omglab.ComparatorInt;
-import by.gsu.omglab.ComparatorString;
 import by.gsu.omglab.Race;
 import by.gsu.omglab.Serelizator;
 
@@ -36,28 +32,21 @@ public class Runner {
 		
 		Serelizator.serialize(races);
 		races = Serelizator.deSerialize();
+	
+		System.out.println("--------------------");
+		printAll(races);
+		System.out.println("--------------------");
 		
-	int total=0;
-	int prizes=0;
-	final ComparatorInt comp1= new ComparatorInt();
-	final ComparatorString comp2 = new ComparatorString();
-	for ( Race race:races){
-		total+=race.getRacersNumber();
-		if(race.isHavePrize()){
-			prizes++;
-		}
-	}
-	System.out.println("Total"+total);
-	System.out.println("Races with prize"+prizes);
-	System.out.println("--------------------");
-	printAll(races);
-	System.out.println("--------------------");
-	Arrays.sort(races,comp1);
-	printAll(races);
-	System.out.println("--------------------");
-	Arrays.sort(races,comp2);
-	printAll(races);
-
+		int total=0;
+		int prizes=0;
+		for ( Race race:races){
+			total+=race.getRacersNumber();
+			if(race.isHavePrize()){
+				prizes++;
+			}
+		}	
+		System.out.println("Total"+total);
+		System.out.println("Races with prize"+prizes);
 	}
 
 }
